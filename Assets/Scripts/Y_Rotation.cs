53dae34e37a45ce033de5a1c602579c74068ff6b
bysen32 <bysen32@gmail.com>
2017-03-16 15:32:14 +0800

    Finish Chapter7 Practice

---
+++ Assets/Scripts/Y_Rotation.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Y_Rotation : MonoBehaviour {
+    public float speed = 3;
+    void FixedUpdate() {
+        transform.Rotate(new Vector3(0, speed, 0));
+    }
+}
